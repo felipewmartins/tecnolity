e29ad9529689bf2fc9ba4113517647d704738cff
eprogramming <00hf11@gmail.com>
2015-03-29 23:10:59 -0300

    more groovy sql goodness

--- src/main/groovy/org/esmerilprogramming/tecnolity/util/Conexao.groovy
+++ src/main/groovy/org/esmerilprogramming/tecnolity/util/Conexao.groovy
@@ -3,31 +3,56 @@ package org.esmerilprogramming.tecnolity.util
 import java.sql.*
 import groovy.sql.*
 
+@Singleton
 class Conexao implements ConexaoDB {
   Connection conexao
   Statement expressao
   ResultSet resultado
-  String driver
-  String fonteDados
-  String usuario
-  String senha
 
   def db
 
-  Conexao() {
+  void setupDB() {
+
     db = Sql.newInstance('jdbc:h2:mem:tecnolity', 'sa', '', 'org.h2.Driver')
-  }
 
-  Conexao(String driver, String fonteDados) {
-    this.driver = driver
-    this.fonteDados = fonteDados
-  }
+    db.execute '''
+create table pais ( pais_id int(3) not null auto_increment,
+  sigla varchar(3) not null, nome varchar(50) not null, primary key (pais_id)
+);
+'''
 
-  Conexao(String driver, String fonteDados, String usuario, String senha) {
-    this.driver = driver
-    this.fonteDados = fonteDados
-    this.usuario = usuario
-    this.senha = senha
+    db.execute '''
+create table usuario (
+  usuario_id int(4) not null auto_increment,
+  nome varchar(30) not null,
+  senha varchar(30) not null,
+  sexo tinyint(1) not null,
+  nome_completo varchar(60) not null,
+  identidade varchar(15) not null,
+  orgao_emissor_rg varchar(10) not null,
+  cpf varchar(11) not null,
+  departamento int(2) null,
+  logradouro varchar(100) not null,
+  complemento varchar(50) null,
+  bairro varchar(50) not null,
+  cidade varchar(50) not null,
+  estado varchar(3) null,
+  cep varchar(8) null,
+  ddd varchar(3) null,
+  telefone varchar(8) null,
+  ramal varchar(8) null,
+  celular varchar(8) null,
+  email varchar(60) null,
+  senha_alterada tinyint(1) null,
+  primary key (usuario_id)
+);
+'''
+
+   def params = [1, 'e', 'p', 1, 'Esmeril Programming', '999999999', 'ssp', '9999999999',
+   1,'universe','orion','space','gamma','ray','12345','111','88888888','111','88888888','ep@test.com', 0]
+
+   db.execute 'insert into usuario values (?, ?, ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', params
+
   }
 
   void abrirConexao() throws Exception {
@@ -68,20 +93,4 @@ class Conexao implements ConexaoDB {
     quantAtualizacoes
   }
 
-  static main(args) {
-
-    def db = Sql.newInstance('jdbc:h2:mem:tecnolity', 'sa', '', 'org.h2.Driver')
-
-    db.execute '''
-create table pais (
-  pais_id int(3) not null auto_increment,
-  sigla varchar(3) not null,
-  nome varchar(50) not null,
-  primary key (pais_id)
-);
-'''
-    def paises = db.rows('select * from pais')
-    println paises
-
-  }
 }
