28817d8622cab47deaa47365976f8918a550c06f
eprogramming <00hf11@gmail.com>
2015-03-29 21:36:22 -0300

    it is running ;]

--- src/main/groovy/org/esmerilprogramming/tecnolity/aplicacao/FormAutenticacao.groovy
+++ src/main/groovy/org/esmerilprogramming/tecnolity/aplicacao/FormAutenticacao.groovy
@@ -6,61 +6,61 @@ import javax.swing.*
 import org.esmerilprogramming.tecnolity.administracao.*
 import org.esmerilprogramming.tecnolity.administracao.ui.*
 
-class FormAutenticacao extends JDialog implements ActionListener
-{
+class FormAutenticacao extends JDialog implements ActionListener {
+
   private JButton btAcessar, btCancelar
-    private JTextField txtUsuario
-    private JPasswordField txtSenha
+  private JTextField txtUsuario
+  private JPasswordField txtSenha
 
-    private GridBagLayout gridbag
-    private GridBagConstraints gbc
+  private GridBagLayout gridbag
+  private GridBagConstraints gbc
 
-    private Aplicacao aplicacao
+  private Aplicacao aplicacao
 
-    FormAutenticacao(Aplicacao aplicacao) {
-      super(aplicacao,true)
-        this.aplicacao = aplicacao
-        Container conteudo = this.getContentPane()
-        conteudo.setLayout(new BorderLayout())
-        JLabel lblImagem = new JLabel(new ImageIcon("imagens/splash.gif"))
-        lblImagem.setSize(400,122)
-        conteudo.add(lblImagem, BorderLayout.NORTH)
+  FormAutenticacao(Aplicacao aplicacao) {
+    super(aplicacao,true)
+    this.aplicacao = aplicacao
+      Container conteudo = this.getContentPane()
+      conteudo.setLayout(new BorderLayout())
+      JLabel lblImagem = new JLabel(new ImageIcon("imagens/splash.gif"))
+      lblImagem.setSize(400,122)
+      conteudo.add(lblImagem, BorderLayout.NORTH)
 
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize()
-        this.setBounds((screenSize.width/2) - 200,(screenSize.height/2) - 150, 410, 300)
+      Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize()
+      setBounds(screenSize.width/2 - 200 as int, screenSize.height/2 - 150 as int, 410, 300)
 
-        gridbag = new GridBagLayout()
-        gbc = new GridBagConstraints()
-        gbc.fill = GridBagConstraints.NONE
-        gbc.anchor = GridBagConstraints.NORTHWEST
-        gbc.insets.bottom = 2
-        gbc.insets.left = 2
-        gbc.insets.right = 2
-        gbc.insets.top = 2
+      gridbag = new GridBagLayout()
+      gbc = new GridBagConstraints()
+      gbc.fill = GridBagConstraints.NONE
+      gbc.anchor = GridBagConstraints.NORTHWEST
+      gbc.insets.bottom = 2
+      gbc.insets.left = 2
+      gbc.insets.right = 2
+      gbc.insets.top = 2
 
-        JPanel pnlAcesso = new JPanel(gridbag)
-        JLabel lblTexto = new JLabel("Usuário: ")
-        adicionarComponente(pnlAcesso, lblTexto, 0, 0, 1, 1)
-        txtUsuario = new JTextField(10)
-        txtUsuario.addActionListener(this)
-        adicionarComponente(pnlAcesso, txtUsuario, 0, 1, 1, 1)
+      JPanel pnlAcesso = new JPanel(gridbag)
+      JLabel lblTexto = new JLabel("Usuário: ")
+      adicionarComponente(pnlAcesso, lblTexto, 0, 0, 1, 1)
+      txtUsuario = new JTextField(10)
+      txtUsuario.addActionListener(this)
+      adicionarComponente(pnlAcesso, txtUsuario, 0, 1, 1, 1)
 
-        lblTexto = new JLabel("Senha: ")
-        adicionarComponente(pnlAcesso, lblTexto, 1, 0, 1, 1)
-        txtSenha = new JPasswordField(10)
-        txtSenha.addActionListener(this)
-        adicionarComponente(pnlAcesso, txtSenha, 1, 1, 1, 1)
+      lblTexto = new JLabel("Senha: ")
+      adicionarComponente(pnlAcesso, lblTexto, 1, 0, 1, 1)
+      txtSenha = new JPasswordField(10)
+      txtSenha.addActionListener(this)
+      adicionarComponente(pnlAcesso, txtSenha, 1, 1, 1, 1)
 
-        conteudo.add(pnlAcesso, BorderLayout.CENTER)
+      conteudo.add(pnlAcesso, BorderLayout.CENTER)
 
-        JPanel pnlComandos = new JPanel(new FlowLayout(FlowLayout.RIGHT))
-        btAcessar = new JButton("Acessar")
-        btAcessar.addActionListener(this)
-        pnlComandos.add(btAcessar)
-        btCancelar = new JButton("Cancelar")
-        btCancelar.addActionListener(this)
-        pnlComandos.add(btCancelar)
-        conteudo.add(pnlComandos, BorderLayout.SOUTH)
+      JPanel pnlComandos = new JPanel(new FlowLayout(FlowLayout.RIGHT))
+      btAcessar = new JButton("Acessar")
+      btAcessar.addActionListener(this)
+      pnlComandos.add(btAcessar)
+      btCancelar = new JButton("Cancelar")
+      btCancelar.addActionListener(this)
+      pnlComandos.add(btCancelar)
+      conteudo.add(pnlComandos, BorderLayout.SOUTH)
     }
 
   private void adicionarComponente(JPanel painel, Component c, int linha, int coluna, int largura, int altura) {
@@ -119,5 +119,5 @@ class FormAutenticacao extends JDialog implements ActionListener
     if(objeto == btCancelar) {
       aplicacao.finalizarAplicacao()
     }
-  }    
+  }
 }
