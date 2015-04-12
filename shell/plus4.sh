a7206baf5366b82ef130582a1de627ff5d2dfa2e
eprogramming <00hf11@gmail.com>
2015-04-12 07:40:20 -0300

    removendo scripts, estão em outro repositório agora

--- shell/plus4.sh
+++
@@ -1,5 +0,0 @@
-for f in $(find . -type f -name '*.groovy'); do
-  mv $f $f.tmp
-  sed -r 's/\+\"/\+ \"/g' $f.tmp > $f
-  rm -f $f.tmp
-done
--- shell/semicolon.sh
+++
@@ -1,5 +0,0 @@
-for f in $(find . -type f -name '*.groovy'); do
-  mv $f $f.tmp
-  sed 's/;//g' $f.tmp > $f
-  rm -f $f.tmp
-done
