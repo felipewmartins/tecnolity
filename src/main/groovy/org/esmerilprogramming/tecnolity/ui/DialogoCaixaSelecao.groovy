61248b9cee9af9a7091c26141e562b2590459d20
eprogramming <00hf11@gmail.com>
2015-04-14 00:12:11 -0300

    updates

--- src/main/groovy/org/esmerilprogramming/tecnolity/ui/DialogoCaixaSelecao.groovy
+++
@@ -1,9 +0,0 @@
-package org.esmerilprogramming.tecnolity.ui
-
-interface DialogoCaixaSelecao {
-  void setVisible(boolean p0)
-  void setAplicacao(Aplicacao p0)
-  void setObjeto(Object p0)
-  void montarInterface()
-  Object getObjeto()
-}
